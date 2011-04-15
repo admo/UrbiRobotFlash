dbeb415fa8d2fec20028f4602e33cc4cb06f9d11
Adam Oleksy <adam.oleksy@gmail.com>
2011-04-15 11:11:11 +0200

    Pierwszy import.

---
+++ src/test.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   test.cpp
+ * Author: aoleksy
+ *
+ * Created on 15 kwiecień 2011, 11:10
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
