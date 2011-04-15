e617f4fa80c8d7d26d001395bcf392adf74b2133
Adam Oleksy <adam.oleksy@gmail.com>
2011-04-15 11:09:51 +0200

    Pierwszy import.

---
+++ src/urobotflash.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   URobotFlash.cpp
+ * Author: aoleksy
+ * 
+ * Created on 15 kwiecień 2011, 11:09
+ */
+
+#include "urobotflash.h"
+
+URobotFlash::URobotFlash() {
+}
+
+URobotFlash::URobotFlash(const URobotFlash& orig) {
+}
+
+URobotFlash::~URobotFlash() {
+}
+
---
+++ src/urobotflash.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   URobotFlash.h
+ * Author: aoleksy
+ *
+ * Created on 15 kwiecień 2011, 11:09
+ */
+
+#ifndef UROBOTFLASH_H
+#define	UROBOTFLASH_H
+
+class URobotFlash {
+public:
+    URobotFlash();
+    URobotFlash(const URobotFlash& orig);
+    virtual ~URobotFlash();
+private:
+
+};
+
+#endif	/* UROBOTFLASH_H */
+
